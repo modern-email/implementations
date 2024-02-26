da0f20dd46290751c3dd78fce982c8a700476441
Mechiel Lukkien <mechiel@ueber.net>
2024-02-26 09:14:32 +0100

    quick fix for popup on page when it has scrolling
    by making the overlay position:fixed
    
    reported by ben

--- index.js
+++ index.js
@@ -706,7 +706,7 @@ const popup = (...kids) => {
 		}
 	};
 	let content;
-	const root = dom.div(style({ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, backgroundColor: 'rgba(0, 0, 0, 0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 1 }), function keydown(e) {
+	const root = dom.div(style({ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0, backgroundColor: 'rgba(0, 0, 0, 0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 1 }), function keydown(e) {
 		if (e.key === 'Escape') {
 			e.stopPropagation();
 			close();
